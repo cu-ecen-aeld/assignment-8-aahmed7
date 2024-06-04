f06ab45623e11e737df246fef244a47cb4186d8d
Arslan Ahmad <arslan.aa7@gmail.com>
2024-06-04 12:07:14 +0500

    use hashes for assignment9
    Signed-off-by: Arslan Ahmad <arslan.aa7@gmail.com>

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,13 +6,16 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'c63d68a3cffecf3d8aa8da483132894de6be0800'
+AESD_ASSIGNMENTS_VERSION = '4336e773c188155954e915397ddd1161fbb6c6bd'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-aahmed7.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+
+# AESD_ASSIGNMENTS_SITE = '/home/vcd/linux-1/assignments-3-and-later-aahmed7'
+# AESD_ASSIGNMENTS_SITE_METHOD = local
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
--- base_external/package/aesd-char-driver/aesd-char-driver.mk
+++ base_external/package/aesd-char-driver/aesd-char-driver.mk
@@ -6,13 +6,16 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_CHAR_DRIVER_VERSION = 'c63d68a3cffecf3d8aa8da483132894de6be0800'
+AESD_CHAR_DRIVER_VERSION = '4336e773c188155954e915397ddd1161fbb6c6bd'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 AESD_CHAR_DRIVER_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-aahmed7.git'
 AESD_CHAR_DRIVER_SITE_METHOD = git
 AESD_CHAR_DRIVER_GIT_SUBMODULES = YES
+
+# AESD_CHAR_DRIVER_SITE = '/home/vcd/linux-1/assignments-3-and-later-aahmed7'
+# AESD_CHAR_DRIVER_SITE_METHOD = local
 
 AESD_CHAR_DRIVER_MODULE_SUBDIRS = aesd-char-driver
 
